a1f042432b8c61cd98289c24c3dcc93f83e6e63c
valentin <vozian.valentin@gmail.com>
2021-07-03 20:33:49 +0300

    bugfix

--- iotic-nodes/api.js
+++ iotic-nodes/api.js
@@ -3,41 +3,70 @@ const W3CWebSocket = require('websocket').w3cwebsocket;
 
 class Api {
     host = "pub-sub-gateway:81"
-    token = "red-secret"
+    // host = "localhost:81";
+    token = "red-secret";
 
-    wsp = new WebSocketAsPromised('ws://' + this.host, {
-        createWebSocket: url => new W3CWebSocket(url),
-        packMessage: data => JSON.stringify(data),
-        unpackMessage: data => JSON.parse(data)
-    })
+    wsp = undefined;
+
+    subscriptions = []
 
     constructor() {
-        this.wsp.open()
-            .then(() => console.log("PubSub connected!"))
-            .catch((e) => console.log(e));
+        this.start()
+    }
+
+    start() {
+
+        this.wsp = new WebSocketAsPromised('ws://' + this.host, {
+            createWebSocket: url => new W3CWebSocket(url),
+            packMessage: data => JSON.stringify(data),
+            unpackMessage: data => JSON.parse(data)
+        })
+
+        this.wsp.open().then(() => {
+            console.log("PubSub connected!");
+
+            let oldSubscriptions = [...this.subscriptions];
+            this.subscriptions = [];
+
+            for(let {topic, callback} of oldSubscriptions){
+                this.subscribe(topic, callback);
+            }
+        })
+
+        this.wsp.onClose.addListener((e) => {
+            console.log("Restarting: " + e);
+            setTimeout(() => this.start(), 2000)
+        });
     }
 
     subscribe(topic, callback) {
-        this.wsp.sendPacked({
-            event: "subscribe", data: {
-                token: this.token, pattern: topic
-            }
-        })
+        this.subscriptions.push({topic,callback})
+        console.log("Subscrbie topic: " + topic);
 
-        this.wsp.onUnpackedMessage.addListener(({data}) => {
-            console.log("data received: " + data)
-            if (data.topic === topic) {
-                callback(data.data)
-            }
-        })
+        if(this.wsp.isOpened) {
+            this.wsp.sendPacked({
+                event: "subscribe", data: {
+                    token: this.token, pattern: topic
+                }
+            })
+
+            this.wsp.onUnpackedMessage.addListener(({data}) => {
+                console.log("data received: " + JSON.stringify(data))
+                if (data.topic === topic) {
+                    callback(data.data)
+                }
+            })
+        }
     }
 
     publish(topic, data) {
-        this.wsp.sendPacked({
-            event: "publish", data: {
-                token: this.token, pattern: topic, data
-            }
-        })
+        if(this.wsp.isOpened) {
+            this.wsp.sendPacked({
+                event: "publish", data: {
+                    token: this.token, pattern: topic, data
+                }
+            })
+        }
     }
 
 }
